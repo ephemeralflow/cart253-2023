5405a6454ccf703d6d5373c7d2d8d48f7ed6e55c
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-09-08 17:10:44 -0400

    Drawing Canvas thing idk how to call this ngl

---
+++ sandboxes/drawing-experiments/js/script.js
@@ -0,0 +1,43 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+    createCanvas(500, 500);
+    background(255, 127, 127);
+    rectMode(CENTER);
+    stroke(0, 0, 255);
+    fill(127, 255, 127);
+    rect(250, 250, 400, 400);
+    fill(255, 255, 127);
+    rect(250, 250, 300, 300);
+    fill(127, 127, 255);
+    rect(250, 250, 200, 200);
+    fill(127, 255, 255);
+    rect(250, 250, 100, 100);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
