01f04017075b52dd2e635f556ba636bd57ad5472
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-09-10 17:05:44 -0400

    Activity 02: Drew an amazing alien!!!

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -20,7 +20,35 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    //Creates the canvas where the alien will be + set the color
+    createCanvas(640, 480);
+    background(255, 127, 127);
 
+    //removes the stroke color
+    noStroke();
+
+    //Making of the body, setting the colour to grey and setting the shape
+    fill(127, 127, 127);
+    ellipse(320, 450, 300, 300);
+
+    //Making of head
+    fill(100, 100, 100);
+    ellipse(320, 170, 200, 300);
+
+    //Making of the eyes
+    fill(0, 0, 0);
+    ellipse(250, 170, 50, 150);
+    ellipse(390, 170, 50, 150);
+
+    //Making the nose
+    ellipse(300, 250, 10, 10);
+    ellipse(330, 250, 10, 10);
+
+    //Making the mouth by enabling the stroke again
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    rectMode(CENTER);
+    rect(320, 290, 100, 50);
 }
 
 
