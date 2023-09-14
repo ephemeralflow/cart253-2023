2bc1d0ebcc0dad7db17e021eaca3367fa5b877c6
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-09-14 18:41:38 -0400

    Variables Practice Finish.

--- sandboxes/variables/js/script.js
+++ sandboxes/variables/js/script.js
@@ -15,12 +15,21 @@ function preload() {
 
 }
 
+let backgroundShade = 0;
+let circle = {
+    x: 0, // Back to 0 to it starts on the left
+    y: 250,
+    size: 100,
+    fill: 255,
+    speed: 5
+}
+
 
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500,500);
 }
 
 
@@ -28,5 +37,14 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(backgroundShade);
+    ellipse(circle.x, circle.y, circle.size);
+    circle.x = circle.x + circle.speed; // Move the circle
+    circle.x = constrain(circle.x, 0, width); // Constrain the circle's x position within 0-width
 
+    /*
+    console.log("circleX is " + circleX); 
+    console.log("circleY is " + circleY);
+    console.log(`circleX: ${circleX}, circleY: ${circleY}, circleSize: ${circleSize}, circleSpeed: ${circleSpeed}`);
+    */
 }
