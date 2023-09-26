4a2de620521afd2fa24dd8cbb1aae3b84e241b80
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-09-25 20:06:14 -0400

    Conditionals (Finish)
    Part of codes, want to save more later. TBC

--- sandboxes/conditionals/js/script.js
+++ sandboxes/conditionals/js/script.js
@@ -8,25 +8,59 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+/* Star Sky
+// We need to know how many stars we want to draw in the sky
+let numStars = 100;
 
-}
-
-
-/**
- * Description of setup
-*/
 function setup() {
-
+  createCanvas(500, 500);
+  // White stroke because we're using point() on black
+  stroke(255);
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+  // Black sky
+  background(0);
+  // randomSeed() lets us make random() predictable: it will generate the same sequence of numbers
+  // each time draw() is called
+  randomSeed(0);
+  // Our for loop counts from 0 to numStars
+  for (let i = 0; i < numStars; i++) {
+    // Choose a random x and y position on the canvas
+    let x = random(0, width);
+    let y = random(0, height);
+    // Draw a point (star) there
+    point(x, y);
+  }
+}*/
 
+// A variable to track whether or not the light is on...
+let lightIsOn = false;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+
+  // Draw a red ellipse you can only see when the light is on!
+  fill(255,0,0);
+  ellipse(250,250,100,100);
+
+  // If the light is off, draw a black rectangle on top of everything
+  // to hide it (make it "dark")
+  // Notice how we check if the light is NOT on by using ! in front of the variable
+  if (!lightIsOn) {
+    fill(0);
+    rect(0,0,width,height);
+  }
+}
+
+function mousePressed() {
+  // When the mouse button is pressed, flip the variable
+  lightIsOn = !lightIsOn;
+  // Notice how we can flip the true/false value by using the NOT operator !
+  // If lightIsOn is true, then ! will make it false
+  // If lightIsOn is false, then ! will make it true
 }
