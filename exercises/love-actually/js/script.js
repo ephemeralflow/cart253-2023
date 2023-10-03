76873110334af79ebe2727a61556fd84bb37e759
Nightingale <144470618+ephemeralflow@users.noreply.github.com>
2023-10-03 15:13:21 -0400

    FINISHED THE EXERCISE PLEASE I DONT WANT TO LOSE THIS FILE AGAIN MY PC SHUT DOWN AHHHHH

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Love Actually (?)
+ * Love (?)
  * Scarlett Perez
  * 
  * I decided to play on the unwanted love and make a simulator on running away from a stalker who loves you which (obviously) you don't love back.
@@ -8,7 +8,7 @@
 
 "use strict";
 
-let circle1 = {
+let user = {
     x: 200,
     y: 300,
     size: 100,
@@ -17,7 +17,7 @@ let circle1 = {
     speed: 5, 
 }
 
-let circle2 = {
+let stalker = {
     x: 400,
     y: 300,
     size: 100,
@@ -26,17 +26,24 @@ let circle2 = {
     speed: 5, 
 }
 
+//Setting the timer name and the images, also the current state
 let timer = 15;
-
 let state = `title`
+let bgimg;
+let ene;
+let youcore;
+
+function preload() {
+    //preloading the images for later
+    bgimg = loadImage ('assets/images/IMG_5452.jpg');
+    ene = loadImage('assets/images/ene.png');
+    youcore = loadImage('assets/images/youcore.png');
+}
 
 function setup() {
+    //Setting up the canvas and the background
     createCanvas(600, 600);
-
-    circle1.vx = random(-circle1.speed, circle1.speed);
-    circle1.vy = random(-circle1.speed, circle1.speed);
-    circle2.vx = random(-circle2.speed, circle2.speed);
-    circle2.vy = random(-circle2.speed, circle2.speed);
+    image(bgimg,0,0);
 }
 
 
@@ -45,7 +52,8 @@ function setup() {
 */
 function draw() {
     background(0);
-    
+    imageMode(CORNERS);
+    image (bgimg, 0,0,600,600)
 
     text(timer, 50, 50);
 
@@ -69,30 +77,17 @@ function draw() {
 
 function title() {
     textSize(64);
-    fill(200,100,100);
+    fill(176,11,30);
     textAlign(CENTER,CENTER);
-    text(`LOVE?`, width/2,height/2);
+    text(`LOVE?`, width/2,260);
     textSize(20);
     textAlign(CENTER,CENTER);
-    text(`Move by holding down the mouse`, width/2,350);
-    text(`button and moving your mouse.`, width/2,380);
-    textSize(10);
+    text(`Move by holding down the mouse\nbutton and moving your mouse.`, width/2,320);
+    
+    fill(102,44,44)
+    textSize(15);
     textAlign(CENTER,CENTER);
-    text(`You're being followed by someone who loves you.`, width/2,400);
-    text(`They believe you love them back.`, width/2,410);
-    text(`You don't know them.`, width/2,420);
-    text(`Don't get caught in this stalker's twisted love.`, width/2,430);
-}
-
-function survival(){
-    textSize(30);
-    if (frameCount % 60 == 0 && timer > 0) {
-        timer --;
-    }
-
-    if (timer == 0) {
-        survived();
-    }
+    text(`You're being followed by someone who loves you.\nThey believe you love them back.\nYou don't know them.\nDon't get caught in this stalker's twisted love.`, width/2,400);
 }
 
 function simulation(){
@@ -104,23 +99,18 @@ function simulation(){
     survival();
 }
 
-function survived() {
-    textSize(64);
-    fill(200,100,100);
-    textAlign(CENTER,CENTER);
-    text(`You escaped!`, width/2,height/2);
-}
-
 function lose() {
+    //Text for bad ending
     textSize(55);
-    fill(200,100,100);
+    fill(176,11,30);
     textAlign(CENTER,CENTER);
     text(`"You love me too right?!"`, width/2,height/2);
 }
 
 function flee() {
+    //Text for the ending that you get from leaving the canvas, neutral ending
     textSize(72);
-    fill(200,100,100);
+    fill(176,11,30);
     textAlign(CENTER,CENTER);
     text(`You ran...`, width/2,height/2);
     textSize(50);
@@ -139,50 +129,79 @@ function flee() {
     text(`...and ran...`, width/2,580);
 }
 
+function survival(){
+    //Setting a timer that would count down for the amont of time the user has to hold out and survive
+    textSize(30);
+    if (frameCount % 60 == 0 && timer > 0) {
+        timer --;
+    }
+
+    if (timer == 0) {
+        survived();
+        noLoop();
+    }
+}
+
+function survived() {
+    background(0);
+    //reinstating the background image because if I didn't the user and stalker would stay on the canvas and I have no idea why but nothing that 2 lines of code can't fix!!!
+    imageMode(CORNERS);
+    image (bgimg, 0,0,600,600)
+    //Text that shows that you won/fled
+    textSize(64);
+    fill(176,11,30);
+    textAlign(CENTER,CENTER);
+    text(`You escaped!`, width/2,height/2);
+}
 function move() {
-    circle2.x = circle2.x + circle2.vx;
-    circle2.y = circle2.y + circle2.vy;
+    //Making it possible for the stalker to move
+    stalker.x = stalker.x + stalker.vx;
+    stalker.y = stalker.y + stalker.vy;
 }
 
 function pursuit() {
-    let dx = circle2.x - mouseX; 
-      let dy = circle2.y - mouseY; 
+    //To make it that the stalker would follow the user's mouse position
+    let dx = stalker.x - mouseX; 
+      let dy = stalker.y - mouseY; 
 
       if (dx < 0) { 
-        circle2.vx = circle2.speed;
+        stalker.vx = stalker.speed;
       }
       else if (dx > 0) { 
-        circle2.vx = -circle2.speed;
+        stalker.vx = -stalker.speed;
       }
       
       if (dy < 0) {
-        circle2.vy = circle2.speed;
+        stalker.vy = stalker.speed;
       }
       else if (dy > 0) {
-        circle2.vy = -circle2.speed;
+        stalker.vy = -stalker.speed;
       }
 }
 
 function checkOffscreen() {
-    //Sad Ending
-    if (circle1.x > width || circle1.x < 0 || circle1.y > height ||circle1.y <0 || circle2.x > width || circle2.x < 0 || circle2.y > height ||circle2.y <0){
+    //Checks if the user circle is off the canvas, giving you the ending of fleeing
+    if (user.x > width || user.x < 0 || user.y > height ||user.y <0 || stalker.x > width || stalker.x < 0 || stalker.y > height ||stalker.y <0){
         state = `flee`;
     }
 }
 
 function checkOverlap() {
-    //True Love!
-    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
-    if (d < circle1.size/2  && d < circle2.size/2 ){
+    //Checks if the user circle and the stalker overlap, making user lose but stalker win
+    let d = dist(user.x, user.y, stalker.x, stalker.y);
+    if (d < user.size/2  && d < stalker.size/2 ){
         state = `lose`;
     } 
 }
 
 function display() {
-    ellipse(circle1.x, circle1.y, circle1.size, circle1.size);
-    ellipse(circle2.x, circle2.y, circle2.size, circle2.size);
+    //Displays the circles as images
+    imageMode(CENTER);
+    image (youcore, user.x, user.y)
+    imageMode(CENTER);
+    image (ene, stalker.x, stalker.y)
 }
-
+//When pressing the mouse, you will start the game
 function mousePressed() {
     if (state === `title`){
         state = `simulation`;
@@ -190,7 +209,7 @@ function mousePressed() {
 }
 
 function mouseDragged() {
-    //Move one circle by dragging the mouse
-    circle1.x = mouseX;
-    circle1.y = mouseY;
+    //Move the user circle by dragging the mouse
+    user.x = mouseX;
+    user.y = mouseY;
     }
