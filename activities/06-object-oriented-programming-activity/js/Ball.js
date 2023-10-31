4ffc20f2dc51e656128d74d69e962db489fd4602
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-10-30 20:39:22 -0400

    Update Ball.js

--- activities/06-object-oriented-programming-activity/js/Ball.js
+++ activities/06-object-oriented-programming-activity/js/Ball.js
@@ -36,12 +36,12 @@ class Ball {
             this.y + this.size/2 > paddle.y - paddle.height/2 &&
             this.y - this.size/2 < paddle.y + paddle.height/2) {
     
-          // Bounce
-          let dx = this.x - paddle.x;
-          this.vx = this.vx + map(dx,-paddle.width/2,paddle.width/2,-2,2);
+            // Bounce
+            let dx = this.x - paddle.x;
+            this.vx = this.vx + map(dx,-paddle.width/2,paddle.width/2,-2,2);
     
-          this.vy = -this.vy;
-          this.ay = 0;
+            this.vy = -this.vy;
+            this.ay = 0;
         }
     }
 
