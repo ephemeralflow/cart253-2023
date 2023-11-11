adb45ca9025a6499a50e468370a2511a1e09d649
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-11-11 09:45:12 -0500

    Activity: Started the sound activity

--- sandboxes/p5.sound/js/script.js
+++ sandboxes/p5.sound/js/script.js
@@ -8,11 +8,17 @@
 
 "use strict";
 
+let barkSFX;
+let oscillator;
+let theramin;
+let synth;
+let notes = [`F4`, `G4`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F5`];
+
 /**
  * Description of preload
 */
 function preload() {
-
+    barkSFX = loadSound(`assets/sounds/bark.wav`);
 }
 
 
@@ -20,7 +26,10 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(600, 600);
+    userStartAudio()
 
+    synth = new p5.PolySynth();
 }
 
 
@@ -28,5 +37,23 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+}
 
+function mousePressed() {
+    synth.play(`C4`, 1, 0, 1);
+    //barkSFX.loop();
+}
+
+function mouseReleased() {
+    theramin.stop();
+}
+
+function keyPressed() {
+    playRandomNote()
+}
+
+function playRandomNote() {
+    let note = random(notes);
+    synth.play(note, 1, 0, 1);
 }
