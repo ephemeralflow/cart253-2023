435680699d3c7d1e963c48fa54acd8167267a884
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-11-12 09:45:40 -0500

    Ex: Started Make Some Noise exercise

--- activities/07-sound-activity/js/script.js
+++ activities/07-sound-activity/js/script.js
@@ -8,6 +8,10 @@
 
 "use strict";
 
+let balls = [];
+
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
+
 /**
  * Description of preload
 */
@@ -20,7 +24,9 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(600, 600);
 
+    userStartAudio();
 }
 
 
@@ -28,5 +34,22 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
+}
+
+function mousePressed() {
+    createBall(mouseX, mouseY);
+}
+
+function createBall(x, y) {
+    let note = random(notes);
+    let ball = new Ball(x, y, note);
+    balls.push(ball);
 }
