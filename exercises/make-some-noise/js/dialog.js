baa48f5add0adaf0716ab0f000d18bfb833ff016
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-11-14 10:18:26 -0500

    EX: Still working on music exercise, added more scenes and added one ending (yay!)

--- exercises/make-some-noise/js/dialog.js
+++ exercises/make-some-noise/js/dialog.js
@@ -1,26 +1,259 @@
 let scenes = [{
-    name: "v",
-    txt: "hello everynyaaaaa",
+    name: " ",
+    txt: "You walked in an elegant room.",
 }, {
-    name: "v",
-    txt: "how are you",
+    name: " ",
+    txt: "Awhile ago, this random lady kinda told you to come with her into this building.",
 }, {
-    name: "v",
-    txt: "fine? zank you",
+    name: " ",
+    txt: "Not really knowing the area and what to do, you obliged.",
 }, {
-    name: "l",
-    txt: ":O",
+    name: " ",
+    txt: "You suppose that it would be incredibly easy to kidnap you if this was all it took but alas, this building was thankfully a very incredibly public one.",
 }, {
-    name: "a",
-    txt: "oh mah gaaawd",
+    name: " ",
+    txt: "And if your knowledge was correct-",
 }, {
-    name: "a",
-    txt: "???",
+    name: " ",
+    txt: "This was Lady Furina, the *practically* god of this country.", //5 SPRITE SHOW
 }, {
-    name: "v",
-    txt: "woot woot",
-}
-    , {
-    name: "v",
-    txt: "yoss",
+    name: " ",
+    txt: "Practically because you don't know anything you lived under the rocks (literally).",
+}, {
+    name: "Furina",
+    txt: "HELLOOOOO! Are you even listening to me anymore?!.",
+}, {
+    name: "You",
+    txt: "Um...", //8 CHOICE
+}, {
+    name: "Furina",
+    txt: "Oh REALLY?", //9
+}, {
+    name: "Furina",
+    txt: "I can literally tell you are lying.", //10
+}, {
+    name: "Furina",
+    txt: `"No"`, //11
+}, {
+    name: "Furina",
+    txt: "*scoff* At least you're honest I GUESS.",
+}, {
+    name: "Furina",
+    txt: "Oh REALLY?", //13
+}, {
+    name: "Furina",
+    txt: "UGH it's fine.",
+}, {
+    name: "Furina",
+    txt: "Listen here, and I'll only say it once.",
+}, {
+    name: "Furina",
+    txt: "I'm acting in a play alright? Big stakes, end of the world if it doesn't happen.",
+}, {
+    name: "Furina",
+    txt: "Problem however, our pianist has vanished and it wasn't because of the serial disappearance cases (Though the police force is trying to figure it out!)",
+}, {
+    name: "Furina",
+    txt: "We were going to get a replacement-",
+}, {
+    name: "Furina",
+    txt: "But guess what..!",
+}, {
+    name: "Furina",
+    txt: "ALL THE PIANISTS IN THIS WHOLE COUNTRY HAVE ALL VANISHED????",
+}, {
+    name: "Furina",
+    txt: "While that definitely raises some concerning points, I don't have enough time to figure it out myself while the police force is doing something about it.",
+}, {
+    name: "Furina",
+    txt: "Because of it, there are no pianist for my play!",
+}, {
+    name: "Furina",
+    txt: "That's where you come in alright?",
+}, {
+    name: "Furina",
+    txt: "Your skill is probably mediocre but you will learn everything in two days, d'accord?",
+}, {
+    name: " ",
+    txt: "You could only look at her in shock as you had no idea what to do.",
+}, {
+    name: " ",
+    txt: "Maybe it would have been better to go do those incredibly shady medical trials for all the riches you desired (if you survived) instead of needing to play the piano for a god.",
+}, {
+    name: " ",
+    txt: "It's intimidating to say the least.",
+}, {
+    name: " ",
+    txt: "Even if you knew how to play the piano, all knowledge of piano have just escaped the empty confines of your cerebral cortex.",
+}, {
+    name: " ",
+    txt: "And Furina didn't look too patient for you to start.",
+}, {
+    name: "Furina",
+    txt: "Don't worry, I'll teach you. It won't be *that* bad.", //30
+}, {
+    name: "Furina",
+    txt: "Let's see how you do.", //31
+}, {
+    name: "Furina",
+    txt: "...",
+}, {
+    name: "You",
+    txt: "...",
+}, {
+    name: "Furina",
+    txt: "...",
+}, {
+    name: "Furina",
+    txt: "WHAT.",
+}, {
+    name: "Furina",
+    txt: "I'm so sorry but you have like the musical skill of a toddler.",
+}, {
+    name: "Furina",
+    txt: "NOT EVEN a toddler can do better than you oh my GOODNESS.",
+}, {
+    name: " ",
+    txt: " ", // 38 CHOICE
+}, {
+    name: "You",
+    txt: "You literally just took me from the streets what do you expect.",
+}, {
+    name: "Furina",
+    txt: "More! Better! You're in the city of music and entertainment.",
+}, {
+    name: "Furina",
+    txt: "Talent!",
+}, {
+    name: "Furina",
+    txt: "Something that you unfortunately lack.",
+}, {
+    name: " ",
+    txt: "You're starting to think you should have done that shady medical trial over doing this but in the same time-", //43
+}, {
+    name: " ",
+    txt: "That implied doing a shady medical trial which has a high chance of failure and the amount of riches haven't fully tempted you (yet).",
+}, {
+    name: " ",
+    txt: "That, however, might change what happens next with Lady Furina because it's suddenly very tempting to get all those riches.",
+}, {
+    name: " ",
+    txt: "After all, it's not like Lady Furina has even told you about compensation.", //46
+}, {
+    name: " ",
+    txt: "You closed your eyes in deep thought.",
+}, {
+    name: " ",
+    txt: "You're not the sharpest tool in the shed when it came to pianos apparently.",
+}, {
+    name: "Furina",
+    txt: "W-",
+}, {
+    name: " ",
+    txt: "Furina's voice broke your thought process.", //50
+}, {
+    name: "Furina",
+    txt: "Why are we in an elevator??????????", //51
+}, {
+    name: " ",
+    txt: "lmao?", //49 (according to my counter???) //52
+}, {
+    name: "Furina",
+    txt: "Um??? YES???", //50
+}, {
+    name: "Furina",
+    txt: "What’s literally wrong with you.",
+}, {
+    name: "Furina",
+    txt: "We just got /tp’ed (teleported) away and you just ask me if it MATTERS?",
+}, {
+    name: "Furina",
+    txt: "This is why you suck at piano jeez", //53 //56
+}, {
+    name: " ",
+    txt: "Furina looked at you incredulously.", //54 //57
+}, {
+    name: " ",
+    txt: "You could tell she expected more of an answer than the letters I D and K.",
+}, {
+    name: " ",
+    txt: "But well, what could she really hope for from someone whom she had just picked off from the streets like a stray kitten?", //56
+}, {
+    name: "Furina",
+    txt: "What even is this music it’s killing my ears I think I might die today just hearing it.", //57 //60
+}, {
+    name: " ",
+    txt: "You looked over at the elevator buttons closely.",
+}, {
+    name: " ",
+    txt: "While you apparently can't play the piano, you know how to press buttons.",
+}, {
+    name: " ",
+    txt: "The question is... which one will you press?",
+}, {
+    name: " ",
+    txt: " ", //64???? Whats wrong with this counter
+}, {
+    name: "You",
+    txt: "Let's go with one.", //65
+}, {
+    name: "You",
+    txt: "What could possibly go wrong.",
+}, {
+    name: " ",
+    txt: "Furina was starting to look visibly nervous, after all, imagine blinking and suddenly you're inside an elevator.",
+}, {
+    name: "Furina",
+    txt: "Sure.",
+}, {
+    name: " ",
+    txt: "...",
+}, {
+    name: " ",
+    txt: "After awhile the doors to the elevator had opened.",
+}, {
+    name: " ",
+    txt: "In front of you was..!",
+}, {
+    name: " ",
+    txt: " ", //73
+}, {
+    name: "Furina",
+    txt: "...", //74
+}, {
+    name: "Furina",
+    txt: "WHAT IS HAPPENING.", //75
+}, {
+    name: "You",
+    txt: "IDK",
+}, {
+    name: "Furina",
+    txt: "DONT 'IDK' ME I'M SO CONFUSED.",
+}, {
+    name: "Furina",
+    txt: "I feel like if we stay here any longer we will be victim to this 'kitchen gun' and I dont want to stay around any longer to find out",
+}, {
+    name: " ",
+    txt: " ", //79 KITCHEN GUN ENDING
+}, {
+    name: "You",
+    txt: "Let's go with two.", //80 BUTTON 2
+}, {
+    name: " ",
+    txt: "The elevator doors opened to a grey landscape.", //81 BG 1
+}, {
+    name: " ",
+    txt: "Without much choice, the two of you stepped out of the elevator.",
+}, {
+    name: " ",
+    txt: "The only light coming from the elevator behind you.",
+}, {
+    name: " ",
+    txt: "... which has just shut it's doors.",
+}, {
+    name: " ",
+    txt: "Suddenly, a light appeared out of seemingly thin air.", //85 BG 2
+}, {
+    name: "...",
+    txt: "...",
 }]
--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -8,14 +8,20 @@
 
 "use strict";
 
-let state = `title`
-let scene = 0;
+let state = `simulation`
+let scene = 47;
 
 let name = " ";
 let txt = " ";
 
+//IMAGES
 let arrow;
+let kitchenGun;
 
+//SPRITES
+let focashock;
+
+//MENU TEXT
 let startText = "Start"
 let galleryText = "Gallery"
 let creditText = "Credits"
@@ -24,25 +30,49 @@ let elevatorMusic;
 let elevatorMusicVolume = 0;
 let elevator;
 
+let kitchenGunSound;
+
+//PIANO
+let synth;
+let notes = [`F4`, `G4`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F5`];
+let currentNote = 0;
+let interval;
+
+//PIANO TIMER
+let endPianoTimer = 0;
+let pianoPartLength = 60 * 5;
+
+//let elevatorMus = false;
+
 /**
  * Description of preload
 */
 function preload() {
     arrow = loadImage('assets/images/right-arrow.png');
+    kitchenGun = loadImage('assets/images/KITCHEN GUN.png');
+
+    focashock = loadImage('assets/images/focasprite1.png');
 
     elevatorMusic = loadSound('assets/sounds/Local Forecast - Elevator.mp3')
+    kitchenGunSound = loadSound('assets/sounds/Kitchen Gun.mp3')
 }
 
-let choice1a = "what."
-let choice1b = "what?"
+let choice1a = "Kinda?"
+let choice1b = "No, not really."
+
+let choice2a = "bruh"
+let choice2b = "..."
+
+let choice3a = "Does it matter?"
+let choice3b = "idk"
 
 /**
  * Description of setup
 */
 function setup() {
     createCanvas(1280, 720);
-
-    backgroundMusic();
+    userStartAudio();
+    synth = new p5.PolySynth();
 }
 
 /**
@@ -67,19 +97,44 @@ function draw() {
         menu();
     }
 
-    checkMusicScene()
+    //checkMusicScene()
 }
 
-function backgroundMusic() {
-    elevatorMusic.play();
-    elevatorMusic.loop();
-    elevatorMusic.setVolume(elevatorMusicVolume);
-    userStartAudio();
+function keyPressed() {
+    if (scene == 31) {
+
+        //THIS IS NOT WORKING 
+        endPianoTimer++;
+        if (endPianoTimer >= pianoPartLength) {
+            scene = 32
+            console.log(endPianoTimer)
+        }
+        //THIS IS NOT WORKING
+        //after some time, that the "piano" plays, it will go to the next scene
+
+
+        // Start our interval, calling playNextNote every 500 milliseconds
+        // Assign the result to interval to remember the interval
+        if (interval === undefined) {
+            interval = setInterval(playNextNote, 500);
+        } else {
+            // If they click when it's playing, clear the interval and set interval
+            // back to undefined to stop play
+            clearInterval(interval);
+            interval = undefined;
+        }
+    }
 }
 
-function checkMusicScene() {
-    if (scene == 5) {
-        elevatorMusicVolume += 1
+function playNextNote() {
+    // Chose the note at the current position
+    let note = notes[currentNote];
+    // Play it
+    synth.play(note, 0.2, 0, 0.4);
+    // Increase the current position and go back to 0 when we reach the end
+    currentNote = currentNote + 1;
+    if (currentNote === notes.length) {
+        currentNote = 0;
     }
 }
 
@@ -173,6 +228,8 @@ function simulation() {
     mainimgs();
     vntext();
     choices();
+    elevatorButtons();
+    cinematicGraphics();
 }
 
 function menu() {
@@ -204,7 +261,7 @@ function mainimgs() {
     push()
     fill(255);
     rectMode(CENTER);
-    rect(130, 465, 100, 50);
+    rect(130, 465, 170, 50);
     rect(width / 2, 600, 1180, 200);
     pop()
     image(arrow, 1100, 550, 100, 100)
@@ -233,7 +290,7 @@ function vntext() {
 function choices() {
 
     // CHOICE TEXT BOXES
-    if (scene == 5) {
+    if (scene == 8) {
         push();
         fill(255, 255, 255, 200);
         rectMode(CENTER);
@@ -249,23 +306,107 @@ function choices() {
         text(choice1b, width / 2, 427)
         pop();
     }
+
+    if (scene == 38) {
+        push();
+        fill(255, 255, 255, 200);
+        rectMode(CENTER);
+        strokeWeight(2)
+        rect(width / 2, 297, 350, 55);
+        rect(width / 2, 417, 350, 55);
+        pop();
+
+        push();
+        textAlign(CENTER)
+        textSize(30)
+        text(choice2a, width / 2, 307)
+        text(choice2b, width / 2, 427)
+        pop();
+    }
+
+    if (scene == 52) {
+        push();
+        fill(255, 255, 255, 200);
+        rectMode(CENTER);
+        strokeWeight(2)
+        rect(width / 2, 297, 350, 55);
+        rect(width / 2, 417, 350, 55);
+        pop();
+
+        push();
+        textAlign(CENTER)
+        textSize(30)
+        text(choice3a, width / 2, 307)
+        text(choice3b, width / 2, 427)
+        pop();
+    }
 }
 
 function mousePressed() {
     allMenuButtons()
+    elevatorButtons()
 
     if (mouseX >= 1100 && mouseX <= 1200 && mouseY >= 550 && mouseY <= 650) {
         scene += 1;
+        console.log(scene)
+
+        if (scene === 49) {
+            // Start the music
+            elevatorMusic.play();
+            // Fade it in
+            elevatorMusic.setVolume(0);
+            elevatorMusic.setVolume(1, 2);
+        }
+
+        if (scene === 72) {
+            // Start the music
+            kitchenGunSound.play();
+            // Fade it in
+            kitchenGunSound.setVolume(0);
+            kitchenGunSound.setVolume(1, 2);
+
+            //Fade out elevator music
+            elevatorMusic.setVolume(0, 2);
+        }
     }
 
     //CHOICE 1
-    if (mouseX >= 490 && mouseX <= 790 && mouseY >= 273 && mouseY <= 327 && scene == 5) {
-        scene = 6;
+    if (mouseX >= 490 && mouseX <= 790 && mouseY >= 273 && mouseY <= 327 && scene == 8) {
+        scene = 9;
     }
 
     //BAD CHOICE 1
-    if (mouseX >= 430 && mouseX <= 850 && mouseY >= 390 && mouseY <= 444 && scene == 5) {
-        scene = 7;
+    if (mouseX >= 430 && mouseX <= 850 && mouseY >= 390 && mouseY <= 444 && scene == 8) {
+        scene = 11;
+    }
+
+    if (mouseX >= 1100 && mouseX <= 1200 && mouseY >= 550 && mouseY <= 650 && scene == 10) {
+        scene = 12;
+    }
+
+    //CHOICE 2
+    if (mouseX >= 490 && mouseX <= 790 && mouseY >= 273 && mouseY <= 327 && scene == 38) {
+        scene = 39;
+    }
+
+    //BAD CHOICE 2
+    if (mouseX >= 430 && mouseX <= 850 && mouseY >= 390 && mouseY <= 444 && scene == 38) {
+        scene = 43;
+    }
+
+    //CHOICE 3
+    if (mouseX >= 490 && mouseX <= 790 && mouseY >= 273 && mouseY <= 327 && scene == 52) {
+        scene = 53;
+    }
+
+    //BAD CHOICE 3
+    if (mouseX >= 430 && mouseX <= 850 && mouseY >= 390 && mouseY <= 444 && scene == 52) {
+        scene = 57;
+    }
+
+    //After choice 3 is finished, take to scene 60 to continue the story and not overlap with what the second choice gives
+    if (mouseX >= 1100 && mouseX <= 1200 && mouseY >= 550 && mouseY <= 650 && scene == 56) {
+        scene = 60;
     }
 }
 
@@ -307,3 +448,67 @@ function allMenuButtons() {
     }
 }
 
+function elevatorButtons() {
+
+    if (scene == 64) {
+        push()
+        fill(255)
+        ellipseMode(CENTER)
+        circle(320, 250, 200)
+        circle(640, 250, 200)
+        circle(960, 250, 200)
+        // circle(320, 550, 200)
+        // circle(640, 550, 200)
+        // circle(960, 550, 200)
+        pop()
+
+        push();
+        textAlign(CENTER)
+        fill(255, 0, 0);
+        textSize(60)
+        text("1", 320, 270)
+        text("2", 640, 270)
+        text("3", 960, 270)
+        pop();
+    }
+
+    // push() //TESTING RECTANGLE
+    // fill(255, 0, 0)
+    // rect(860, 350, 300, 100);
+    // pop()
+
+    //CIRCLE 1
+    if (mouseX >= 220 && mouseX <= 420 && mouseY >= 150 && mouseY <= 350 && mouseIsPressed && scene == 64) {
+        scene = 65;
+    }
+
+    //CIRCLE2
+    if (mouseX >= 540 && mouseX <= 740 && mouseY >= 150 && mouseY <= 350 && mouseIsPressed && scene == 64) {
+        scene = 80;
+    }
+
+    //CIRCLE3
+    if (mouseX >= 860 && mouseX <= 1060 && mouseY >= 150 && mouseY <= 350 && mouseIsPressed && scene == 64) {
+        scene = 43;
+    }
+}
+
+function cinematicGraphics() {
+
+    if (scene >= 72 && scene <= 79) {
+        push();
+        imageMode(CORNER);
+        image(kitchenGun, 0, 0, 1280, 720);
+        pop();
+    }
+
+    if (scene >= 72 && scene <= 79) {
+        push()
+        imageMode(CENTER);
+        image(focashock, width / 2, 500, 390, 961);
+        pop()
+        mainimgs();
+        vntext();
+        choices();
+    }
+}
