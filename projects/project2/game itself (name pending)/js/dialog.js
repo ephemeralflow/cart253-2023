b2d76b53feb2b8c57bb116445bbe1ae282e7424b
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2023-11-20 16:48:25 -0500

    P2: Updated Script
    I REALIZED I NEVER PUSHED THIS FILE SINCE I ADDED SOME DIALOGUE SO HERE IT IS

---
+++ projects/project2/game itself (name pending)/js/dialog.js
@@ -0,0 +1,534 @@
+let scenes = [{
+    name: " ",
+    txt: "Your name is ███.",
+}, {
+    name: " ",
+    txt: "Some days ago, you saw this ad for a shady medical trial.",
+}, {
+    name: " ",
+    txt: "Being slightly (very) tight on cash, you decided to go to the location and do the medical trial.",
+}, {
+    name: " ",
+    txt: "I mean, what could possibly go wrong?",
+}, {
+    name: " ",
+    txt: "...",
+}, {
+    name: " ",
+    txt: "The trip was a long one. It took days to get here.",
+}, {
+    name: " ",
+    txt: "The weather where the supposed trial was supposed to happen was full of snow.",
+}, {
+    name: " ",
+    txt: "Like a storm which if it did any more of a gust you would get blown away.",
+}, {
+    name: " ",
+    txt: "Suddenly, you think you should have eaten more counting as the cold was getting to you and you felt as if you had no more strength to continue any longer.",
+}, {
+    name: " ",
+    txt: "But you were close. So close.",
+}, {
+    name: " ",
+    txt: "It made you wonder… should you turn back..?", //10 CHOICE
+}, {
+    name: " ", //11 CHOICE A
+    txt: "It’s not worth it.",
+}, {
+    name: " ",
+    txt: "If you continued on any longer, you feel like you would get frostbite.",
+}, {
+    name: " ",
+    txt: "Money be damned, you can just go back and get a quick job.",
+}, {
+    name: " ",
+    txt: "Besides, you were starting to get a bad feeling on this trial.",
+}, {
+    name: " ",
+    txt: "Needing to get into a country that’s known to have… a bad reputation to say the least.",
+}, {
+    name: " ",
+    txt: "It’s best to go back.",
+}, {
+    name: " ", //17 CHOICE B
+    txt: "You got this far, it would be almost insulting to yourself if you went back now.",
+}, {
+    name: " ",
+    txt: "And you didn’t want to go back after being forced to learn the piano by some lady.",
+}, {
+    name: " ",
+    txt: "The ad had a code to be able to enter the city. After all, the city was closed off from the general public.",
+}, {
+    name: " ",
+    txt: "So this was the last time you could turn back if you wanted to leave.",
+}, {
+    name: " ",
+    txt: "But you have made your decision before coming close to the gates.",
+}, {
+    name: " ",
+    txt: "And with a quivering voice, you mention the code at the gates and to go inside the city.",
+}, {
+    name: " ",
+    txt: "The worried glances between the guards rose some hairs on the back of your neck but you pushed your sudden fear down.",
+}, {
+    name: " ",
+    txt: "It is too late to be scared about what’s to come. You can’t go back now.",
+}, {
+    name: "Guard",
+    txt: "Hey.", //25 CHOICE 
+}, {
+    name: "You",
+    txt: "What’s up?",
+}, {
+    name: "Guard",
+    txt: "I don’t think you want to do this.",
+}, {
+    name: "Guard",
+    txt: "I mean, we already got a heads up but… really..?",
+}, {
+    name: "You",
+    txt: "What.",
+}, {
+    name: "Guard",
+    txt: "A medical trial with The Doctor.",
+}, {
+    name: "Guard",
+    txt: "I don't think you know what you are getting yourself into.",
+}, {
+    name: "Guard",
+    txt: "How about this, I don’t work under him, if you want I can sneak you out-",
+}, {
+    name: "You",
+    txt: "It’s ok.",
+}, {
+    name: " ",
+    txt: "You responded in a slight whisper, you need the money.",
+}, {
+    name: "Guard",
+    txt: "He tortures people..!",
+}, {
+    name: " ",
+    txt: "The guard’s voice raised higher than a whisper. Trying to put some sense into you.",
+}, {
+    name: " ",
+    txt: "Of the little that you’ve heard of this… Doctor. It isn’t anything good. After all, he doesn’t have a good reputation.",
+}, {
+    name: " ", //38
+    txt: "And now your suspicions have been cleared with what the guard had told you right now that the trial is indeed with The Doctor.",
+}, {
+    name: "Guard", //39 CHOICE 3B
+    txt: "I know I’m right...",
+}, {
+    name: "Guard",
+    txt: "I promise you, you would have not liked what would have happened.",
+}, {
+    name: " ",
+    txt: "Besides, he wasn’t notified yet, which means you are free to go.",
+}, {
+    name: " ",
+    txt: "The guard took you to a place where there were barely any guards, pushing you slightly as he grabbed onto his rifle.",
+}, {
+    name: "Guard",
+    txt: "I’ll pretend that you broke from my grasp and chase you for a while, take the opportunity to flee!",
+}, {
+    name: " ",
+    txt: "You nodded, the instant regret of coming here was all that you needed to just go back home.",
+}, {
+    name: " ",
+    txt: "You already missed the food from your hometown, not planning on going back to where you had originally come from.",
+}, {
+    name: " ",
+    txt: "You realized that not all the soldiers in this country were bad people. However, did they help you out of the kindess of their heart, or a place of fear?",
+}, {
+    name: " ", //47 CHOICE 3B END
+    txt: " ",
+}, {
+    name: "Guard",
+    txt: "If you say so.", //48 CHOICE 3A
+}, {
+    name: " ",
+    txt: "The guard looked at you for a while before looking in front once more.",
+}, {
+    name: " ",
+    txt: "You didn’t know what to say, and it’s not like you wanted to do small talk with anyone.",
+}, {
+    name: " ",
+    txt: "The walk was silent as the guard took you to a building.",
+}, {
+    name: " ",
+    txt: "The first thing you heard was a scream.",
+}, {
+    name: "Guard", //53 special txt
+    txt: "Don’t say I didn’t warn you.",
+}, {
+    name: " ",
+    txt: "The guard didn’t even need to announce his appearance as someone approached.",
+}, {
+    name: "???",
+    txt: "Hello random guard, a~nd who might you be?",
+}, {
+    name: " ",
+    txt: "The person looked directly at you, matching your gaze.",
+}, {
+    name: " ",
+    txt: "Upon closer inspection, you see dried blood (or at least you hope it’s dry, you don’t normally look at blood. Scratch that you hope it’s not blood at all) on their coat.",
+}, {
+    name: "Guard",
+    txt: "A volunteer for The Doctor’s experiment.",
+}, {
+    name: "???",
+    txt: "Oh?",
+}, {
+    name: " ",
+    txt: "You nodded in response.",
+}, {
+    name: "???",
+    txt: "The Doctor will LOVE this. It’ll make his day!",
+}, {
+    name: "???",
+    txt: "Thank you, random guard, I’ll take it from here~!",
+}, {
+    name: " ",
+    txt: "The guard nodded in response, looking at you for a while in silence.",
+}, {
+    name: "Guard", //64
+    txt: "Good luck.",
+}, {
+    name: " ",
+    txt: "You saw the guard leave the building before looking back to the one right in front of you.",
+}, {
+    name: "???",
+    txt: "You seem a bit nervous, don’t worry your pretty little head right now!",
+}, {
+    name: "V",
+    txt: "I’m V. I’m the one who works on the machinery in the lab.",
+}, {
+    name: "V",
+    txt: "Well, I made most of them.",
+}, {
+    name: "V",
+    txt: "When I first started here I practically replaced all of them with more effective ones.",
+}, {
+    name: " ",
+    txt: "V was trying to do small talk, maybe that was their own way of trying to calm you down.",
+}, {
+    name: " ",
+    txt: "Their voice, albeit the amount of vocal fluctuation it had (clearly by excitement) still served to calm you down somehow.",
+}, {
+    name: " ",
+    txt: "Perhaps it was because of how casually they were talking to you.",
+}, {
+    name: "V",
+    txt: "The Doctor is working right now but he won’t mind if we just go in his office.",
+}, {
+    name: "V",
+    txt: "After all, he gets interrupted by himself quite a lot",
+}, {
+    name: "V",
+    txt: "Either way, The Doctor is doing paperwork for the Regrator as he always needs to borrow money from him.",
+}, {
+    name: "V",
+    txt: "I’ll be honest, I was the same with this singer some years ago. Though she passed away a long time ago.",
+}, {
+    name: "V", //77
+    txt: "Either way, I feel like us scientists–doctors, we constantly need money for all the expensive equipment we need.",
+}, {
+    name: "V", //78 CHOICE 4A
+    txt: "I’m glad you think so~",
+}, {
+    name: "V",
+    txt: "While broke-ness can be seen as such a bad thing. It’s only natural that we would need cha-ching!",
+}, {
+    name: " ",
+    txt: "The sound effect that V did was so unexpected that you almost choked on air.",
+}, {
+    name: " ",
+    txt: "V however, looked amused by your reaction, a grin plastered on their face.",
+}, {
+    name: " ",
+    txt: "It wasn’t long before they turned to the door to knock on it.", //82
+}, {
+    name: " ", //83 CHOICE 4B
+    txt: "You didn’t respond to what V had to say.",
+}, {
+    name: " ",
+    txt: "With that slip-off, at least you think it is with the sudden change to doctors.",
+}, {
+    name: " ",
+    txt: "It made you slightly nervous to say the least.",
+}, {
+    name: " ", //86
+    txt: "V didn’t look like they minded your silence though, choosing to knock on the door instead.",
+}, {
+    name: "V", //87
+    txt: "Hey boss, I got a surprise for you!",
+}, {
+    name: " ",
+    txt: "You almost heard a sigh coming from the other side.",
+}, {
+    name: "???",
+    txt: "Come in.",
+}, {
+    name: " ",
+    txt: "Walking into the room, you were greeted by who you guessed was The Doctor sitting at a table as he got up from his chair.",
+}, {
+    name: "Doctor",
+    txt: "What have you brought to me V?",
+}, {
+    name: "V",
+    txt: "Remember that ad that was put out for that procedure?",
+}, {
+    name: "V",
+    txt: "This is a LOVELY volunteer for the project!",
+}, {
+    name: " ",
+    txt: "The Doctor didn’t immediately respond, instead a grin played on his lips.",
+}, {
+    name: "Doctor",
+    txt: "Really?",
+}, {
+    name: "V",
+    txt: "Really!",
+}, {
+    name: "V",
+    txt: "That’s right, right?",
+}, {
+    name: " ", //98 CHOICE 5
+    txt: "V looked at you, waiting for you to say your response.",
+}, {
+    name: " ", //99 CHOICE A
+    txt: "V laughed slightly as The Doctor nodded at V.",
+}, {
+    name: " ", //100
+    txt: "The Doctor's smile was almost unsettling but you forced those feelings of fear down.",
+}, {
+    name: "V", //101 CHOICE B
+    txt: "Awwww our subject is a lil’ jokester!",
+}, {
+    name: " ",
+    txt: "The Doctor walked back to his desk as he grabbed a paper from a cabinet.",
+}, {
+    name: "Doctor",
+    txt: "I’m very pleased by this outcome. V, you may leave, I’ll take it from here.",
+}, {
+    name: "V",
+    txt: "Of course boss.",
+}, {
+    name: " ",
+    txt: "V nodded at you, putting a hand on your shoulder as they did a quick thumbs up with their other hand with a smile.",
+}, {
+    name: "Doctor",
+    txt: "Here is the contract. Of course, for legal reasons, I have to do this.",
+}, {
+    name: "Doctor",
+    txt: "Since you have volunteered, I’ll be transparent with you.",
+}, {
+    name: "Doctor",
+    txt: "The moment you sign this contract, you may not leave until the procedure is done.",
+}, {
+    name: "Doctor",
+    txt: "The procedure is a painful one, though I will provide adequate painkillers. Of course, there is a risk of death.",
+}, {
+    name: "Doctor",
+    txt: "If you survive the procedure, you will be thoughtfully rewarded which would be enough to survive the rest of your life.",
+}, {
+    name: "Doctor",
+    txt: "As after this procedure, there will be lots of your needs that will be… eradicated.",
+}, {
+    name: " ",
+    txt: "You looked at the contract while he spoke to you.",
+}, {
+    name: " ",
+    txt: "It was a typical contract, after all, what did you expect?",
+}, {
+    name: "Doctor", //114 CHOICE 5
+    txt: "Do you have any questions?",
+}, {
+    name: "Doctor", //115 CHOICE A
+    txt: "Your body parts will be replaced by machine.",
+}, {
+    name: "Doctor",
+    txt: "Imagine it like… a new cure for when humans have sickly organs.",
+}, {
+    name: "Doctor",
+    txt: "If this is successful, you will be an amazing asset.",
+}, {
+    name: "Doctor", //118 CHOICE B
+    txt: "We won’t know unless you go through with the procedure.",
+}, {
+    name: "Doctor",
+    txt: "Don't worry I'll treat you with outmost carefulness.",
+}, {
+    name: "Doctor",
+    txt: "No matter what happens I'll make sure all your vitals are always at a safe level.",
+}, {
+    name: "Doctor", //121
+    txt: "Good.",
+}, {
+    name: "Doctor", //122
+    txt: "Now… will you sign the contract?",
+}, {
+    name: "You",
+    txt: "Actually I don’t think I want to do this.",
+}, {
+    name: "Doctor",
+    txt: "Hm, most unfortunate.",
+}, {
+    name: "Doctor",
+    txt: "But have you thought that maybe your fate was sealed the moment you stepped inside the country?",
+}, {
+    name: "Doctor", //126 END 3
+    txt: "Maybe you won’t do this procedure, but we always need more bodies for other experiments.",
+}, {
+    name: " ", //127 END
+    txt: " ",
+}, {
+    name: " ", //128 CHOICE A
+    txt: "The Doctor grinned seeing you sign your name on the contract.",
+}, {
+    name: "Doctor",
+    txt: "Good, very good.",
+}, {
+    name: "Doctor",
+    txt: "As much as I would adore to start immediately.",
+}, {
+    name: "Doctor",
+    txt: "There should be some medical tests to get your current vitals so we can compare them to after your procedure.",
+}, {
+    name: "Doctor",
+    txt: "While this surgery is risky, I’m confident that you will survive.",
+}, {
+    name: "Doctor",
+    txt: "After all, it’s not every day that we get to experiment on a volunteer. How perfect.",
+}, {
+    name: " ",
+    txt: "The Doctor got up from his seat, urging you to follow him.",
+}, {
+    name: " ",
+    txt: "You both walked in the hallways for a while before he got the attention of what you presumed to be one of his assistants.",
+}, {
+    name: "Doctor",
+    txt: "Mobius.",
+}, {
+    name: "Mobius",
+    txt: "Doctor.",
+}, {
+    name: " ",
+    txt: "There felt to be some sort of tension between the two. Rather, they just both looked seriously at each other.",
+}, {
+    name: "Doctor",
+    txt: "Please take this new subject to a clean experimentation room, I want you to do basic medical tests on the subject.",
+}, {
+    name: " ",
+    txt: "Mobius crossed her arms, she looked like she was in the middle of an experiment herself.",
+}, {
+    name: "Mobius",
+    txt: "Can't you see I'm busy with another experiment?",
+}, {
+    name: " ",
+    txt: "The Doctor straightened up, crossing his own arms.",
+}, {
+    name: "Doctor",
+    txt: "It wasn't a suggestion Mobius.",
+}, {
+    name: "Doctor",
+    txt: "I want this subject to be treated at least somewhat decently and you know how the other assistants are.",
+}, {
+    name: " ",
+    txt: "Mobius gave a brief nod as she rolled her eyes. Begrudgingly putting down the test tubes in her hands.",
+}, {
+    name: "Mobius",
+    txt: "Follow me.",
+}, {
+    name: " ",
+    txt: "You followed Mobius closely behind her. She was definitely less approachable V. Rather you were starting to miss V already and their cheerful nature.",
+}, {
+    name: "You",
+    txt: "What did the Doctor mean with you being the only one that could?",
+}, {
+    name: "Mobius",
+    txt: "He meant that I was the only one that could treat you humanly. Surprinsingly.",
+}, {
+    name: " ",
+    txt: "Immediately you felt alarm bells playing in your head. Humanly? How were the other assistants for them to not want to treat people as… people?",
+}, {
+    name: "Mobius", //151
+    txt: "It won’t be long, I’ll just jot down your health to prepare for tomorrow.",
+}, {
+    name: " ",
+    txt: " ",
+}, {
+    name: " ",
+    txt: "DAY 2",
+}, {
+    name: " ",
+    txt: "But you were close. So close.",
+}, {
+    name: " ",
+    txt: "But you were close. So close.",
+}]
+
+let day3 = [{
+    name: "You",
+    txt: "W-",
+}, {
+    name: " ",
+    txt: "You looked at yourself. Your own body felt foreign to itself.",
+}, {
+    name: "You",
+    txt: "What did you do to me?!",
+}, {
+    name: "Doctor",
+    txt: `"What did I do" to you? ha ha.`,
+}, {
+    name: " ",
+    txt: "A dark chuckle escaped from The Doctor's lips.",
+}, {
+    name: "Doctor",
+    txt: "I’m pretty sure I had already told you then, subject.",
+}, {
+    name: " ",
+    txt: "The Doctor did indeed tell you before the actual surgery but-",
+}, {
+    name: " ",
+    txt: "Blood spilled from your lips, cascades of crimson tainting both your clothes and the environment.",
+}, {
+    name: " ",
+    txt: "The Doctor, however, did not seem to mind much.",
+}, {
+    name: "You", //maybe make it a choice or respond snappy or ...  idk 
+    txt: "My body is rejecting your stupid parts! You said I would be fine!",
+}, {
+    name: "Doctor",
+    txt: "Mostly fine, subject. You're so noisy, how about you learn to keep quiet.",
+}, {
+    name: " ",
+    txt: "He grabbed a nearby syringe, you already wanted to look away with how comically large the needle looked. Not like this is the first syringe The Doctor had injected into you.",
+}, {
+    name: " ",
+    txt: "The Doctor hit the needle of the syringe a couple of times before he tilted your head to look to the side, to him, as he put the syringe into your arm in a place you wouldn’t be able to see",
+}, {
+    name: " ", //im lazy to add the italics actually
+    txt: "How *nice* of him.",
+}, {
+    name: "Doctor",
+    txt: "Seeing as you have been a pleasant subject all this time. I’ll let your “lashing out” pass as I haven’t given you a pain killer until now.",
+}, {
+    name: "Doctor",
+    txt: "Though you are the finest specimen I have worked on yet, you have survived the surgery and are still “breathing” as a human.",
+}, {
+    name: " ",
+    txt: "The doctor did a low chuckle as he stepped away from you, choosing to walk around you, looking you up and down.",
+}, {
+    name: " ",
+    txt: "Then, you felt hands grab your shoulders. You would have missed it if you hadn’t become so hyper-sensitive to the environment.",
+}, {
+    name: "Doctor",
+    txt: "Your body isn’t rejecting the parts I have given you, dear subject.",
+}, {
+    name: " ",
+    txt: "Your body is rejecting itself!",
+}, {
+    name: " ",
+    txt: "END DAY 3",
+}]
